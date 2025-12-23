b7c810ff780e7b426c3bcf72515c8befe4a11dcf
anyaohadaniella <anyaohadaniella@gmail.com>
2025-12-23 12:41:23 +0100

    Initial commit

--- script.js
+++ script.js
@@ -8,9 +8,15 @@ addTaskBtn.addEventListener("click", () => {
     const taskText = document.createElement("p");
     todoWrapper.classList.add("todo-container");
     deleteButton.classList.add("delete-button");
+    deleteButton.textContent = "DELETE";
+    
     taskText.classList.add("task-text");
     taskText.textContent = task.value;
     todoWrapper.append(taskText);
     todoWrapper.append(deleteButton);
     taskContainer.append(todoWrapper);
+    deleteButton.addEventListener("click", () => {
+        todoWrapper.remove();
+    });
+    task.value = "";
 });
